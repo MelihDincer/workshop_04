c8b60a415b6ceeaca0c8247a7717ba1a0d9ce5c8
Melih Dincer <m_dincer41@hotmail.com>
2025-04-17 20:59:15 +0300

    16 Nisan first commit.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import StarRating from "./StarRating.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={6} size={30} />
+    <StarRating maxRating={10} size={25} />
+    <StarRating />
+  </StrictMode>
+);
